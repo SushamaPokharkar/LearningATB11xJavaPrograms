61b79d2032486fcde373c084c8403fb7fe64b0c0
Vinayak <vvpokharkar@gmail.com>
2025-03-13 22:02:12 -0400

    Test

--- src/org/practice/FirstProgram.java
+++ src/org/practice/FirstProgram.java
@@ -1,7 +1,13 @@
 package org.practice;
 
 public class FirstProgram {
+
     public static void main(String[] args) {
-        System.out.println("Hello Sushama");
+
+        int a=10;
+      //  int b=a++;
+        System.out.println(a++ + ++a);
+        System.out.println(a);
+        System.out.println("You are the best sush darling");
     }
 }
