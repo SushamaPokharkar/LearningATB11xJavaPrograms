b03b6fddd9b16b3f53b0505bc505595acfca5719
Vinayak <vvpokharkar@gmail.com>
2025-03-27 22:22:43 -0400

    Test

--- src/org/practice/FirstProgram.java
+++ src/org/practice/FirstProgram.java
@@ -4,10 +4,8 @@ public class FirstProgram {
 
     public static void main(String[] args) {
 
-        int a=10;
-      //  int b=a++;
-        System.out.println(a++ + ++a);
-        System.out.println(a);
-        System.out.println("You are the best sush darling");
+        for (int i = 0; i > 10; ) {
+            System.out.println("You are the best sush darling");
+        }
     }
 }
